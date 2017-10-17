2d712dc551d26de4c3648c63851b9ef457d91ce3
L kabeer <ahmed@Lkabeer.com>
2017-10-17 13:27:23 +0200

    version 1.3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,7 +26,7 @@ export class HomeComponent implements OnInit {
   }
 
   activeDataTable() {
-    $('#friendyCar').DataTable(
+    var table = $('#friendyCar').DataTable(
       {
         "processing": true,
         // "serverSide": true,
@@ -48,6 +48,29 @@ export class HomeComponent implements OnInit {
         ]
       } 
     );
+
+    /* Custom filtering function which will search data between two values */
+    $.fn.dataTable.ext.search.push(
+      function( settings, data, dataIndex ) {
+          var min = parseInt( $('#min').val(), 10 );
+          var max = parseInt( $('#max').val(), 10 );
+          var age = parseFloat( data[3] ) || 0; // use data for the age column
+   
+          if ( ( isNaN( min ) && isNaN( max ) ) ||
+               ( isNaN( min ) && age <= max ) ||
+               ( min <= age   && isNaN( max ) ) ||
+               ( min <= age   && age <= max ) )
+          {
+              return true;
+          }
+          return false;
+      }
+    );
+
+    // Event listener to the two range filtering inputs to redraw on input
+    $('#min, #max').keyup( function() {
+      table.draw();
+    } );
   }
 
   ngOnInit() {
